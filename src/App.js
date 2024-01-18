dc7fa1a4257696ae36617c4b8322b45cccd77fda
Bernardo Salgueiro <bsalgueiro@gmail.com>
2024-01-18 18:19:59 +0000

    Changes in the Components
    Text and mage changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Message from './components/Message'
 function App() {
   return (
     <div className="App">
-      {/* <Profile name="Bernardo" lastname="Salgueiro"/> */}
-      <Message content="This is an automatic potato."></Message>
+      <h1>This is an Automatic Potato ©</h1>
+      <img width="504" height="252" src="https://www.alimentarium.org/sites/default/files/media/image/2017-02/AL027-01_pomme_de_terre_0_0.jpg"/>
+      <p>This Automatic Potato © has been enhanced with AI and machine learning technologies.</p>
+      <p>This Automatic Potato is the ultimate potato, surrender to the Automatic Potato ©.</p>
     </div>
   );
 }
